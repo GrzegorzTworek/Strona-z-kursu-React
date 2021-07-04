59f287833f7814968f935e6d102fcdd238f39573
GrzegorzTworek <49164265+GrzegorzTworek@users.noreply.github.com>
2021-07-04 16:44:44 +0200

    Add files via upload

---
+++ build/precache-manifest.487d4c4dea70063a499351510421fdde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0002217650a1ac33897e",
+    "url": "/static/css/main.bef17bd0.chunk.css"
+  },
+  {
+    "revision": "0002217650a1ac33897e",
+    "url": "/static/js/main.00022176.chunk.js"
+  },
+  {
+    "revision": "ad0da7a9f90f6d91d9d2",
+    "url": "/static/js/1.ad0da7a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "7e92576a8ac4b52e756c0143c5e6ccc4",
+    "url": "/index.html"
+  }
+];
